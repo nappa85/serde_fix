b5064ad3e5452557e132c1fdd2f923296486249e
Marco Napetti <nappa85@therocktrading.com>
2021-03-25 17:54:06 +0100

    Export checked functions

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,6 @@ pub mod entities;
 const CHECKSUM_MOD: usize = 256;
 
 #[doc(inline)]
-pub use crate::de::{from_bytes, from_reader, from_str, Deserializer};
+pub use crate::de::{from_bytes, from_bytes_checked, from_reader, from_reader_checked, from_str, from_str_checked, Deserializer};
 #[doc(inline)]
 pub use crate::ser::{to_string, to_string_checked, Serializer};
